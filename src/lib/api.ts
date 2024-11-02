cf4130a7b4d73f943ae4ac1a5dee71c55c5cb976
ectuser <m.ectuser@gmail.com>
2024-11-02 18:04:21 +0100

    refactor: remove api related rxjs code

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,4 @@
-import { catchError, delay, of, startWith, switchMap } from "rxjs";
-import { fromFetch } from "rxjs/fetch";
-import { fromPromise } from "rxjs/internal/observable/innerFrom";
+export function request<TResult>(url: string) {}
 
 export type SuccessResponse<TResult> = {
   data: TResult;
@@ -19,41 +17,3 @@ export type LoadingResponse = {
   loading: true;
   error: undefined;
 };
-
-export function request<TResult>(url: string) {
-  return fromFetch(url).pipe(
-    switchMap((response) => {
-      if (response.ok) {
-        return fromPromise(
-          response.json().then(
-            (data) =>
-              ({
-                data,
-                loading: false,
-                error: undefined,
-              } as SuccessResponse<TResult>)
-          )
-        ).pipe(delay(2000));
-      } else {
-        return of({
-          data: undefined,
-          loading: false,
-          error: `Error ${response.status}`,
-        } as ErrorResponse);
-      }
-    }),
-    catchError((err) => {
-      console.error(err);
-      return of({
-        data: undefined,
-        loading: false,
-        error: err.message,
-      } as ErrorResponse);
-    }),
-    startWith({
-      data: undefined,
-      loading: true,
-      error: undefined,
-    } as LoadingResponse)
-  );
-}
