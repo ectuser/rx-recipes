fcc7a3cf1d26ac43ca7dc5b916e43f7b3d5b5d7c
ectuser <m.ectuser@gmail.com>
2024-11-02 18:21:55 +0100

    refactor: add api factory methods

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,31 @@
 export function request<TResult>(url: string) {}
 
+function createSuccessResponse<TResult>(
+  data: TResult
+): SuccessResponse<TResult> {
+  return {
+    data,
+    loading: false,
+    error: undefined,
+  };
+}
+
+function createErrorResponse(error: string): ErrorResponse {
+  return {
+    data: undefined,
+    loading: false,
+    error,
+  };
+}
+
+function createLoadingResponse(): LoadingResponse {
+  return {
+    data: undefined,
+    loading: true,
+    error: undefined,
+  };
+}
+
 export type SuccessResponse<TResult> = {
   data: TResult;
   loading: false;
